475a44e4d21351e9ede51f09e484ff1751e274b8
Jose Antonio Rubio <jose.rubio@gambyt.com>
2023-06-27 15:21:16 -0400

    importing image list to app

---
+++ client/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ImageList from './components/ImageList';
+
+const App = () => {
+  return (
+    <>
+      <ImageList />
+    </>
+  );
+};
+
+export default App;
